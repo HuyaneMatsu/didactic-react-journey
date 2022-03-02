7b1d639dd5540107fa37364530fbed6edffab43b
HuyaneMatsu <re.ism.tm@gmail.com>
2022-03-02 15:31:23 +0100

    clean up derpy js code

--- day_1/static/app.js
+++ day_1/static/app.js
@@ -4,26 +4,40 @@ function $(element_id) {
 
 var create_element = React.createElement;
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
+class LikeButtonState {
+    constructor() {
+        this.liked = false;
+    }
+}
 
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = new LikeButtonState();
+    }
+    
+    handle_click() {
+        this.state.liked = true;
+        this.forceUpdate();
     }
 
-    return create_element(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return create_element(
+            'button',
+            {
+                onClick: () => this.handle_click(),
+            },
+            'Like',
+        );
+    }
 }
 
 ReactDOM.render(
     create_element(LikeButton),
-    $('root'),
+    $('button'),
 );
